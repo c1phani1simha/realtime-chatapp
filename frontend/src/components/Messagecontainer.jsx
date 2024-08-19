44cd8232c28818b4e431e7fd2365bbf9e4e841c1
Chakka Phani Simha <chakkaphanisimha@gmail.com>
2024-08-19 15:03:55 +0530

    added all the message containers sub componenets and finished the Home component

--- frontend/src/components/Messagecontainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Messagecontainer = () => {
-  return (
-    <div>Messagecontainer</div>
-  )
-}
-
-export default Messagecontainer
---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Message from './Message';
+
+const Messages = () => {
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
+}
+
+export default Messages
+//basic code for the messages
+
+
+// import React from 'react'
+// import Message from './Message';
+
+// const Messages = () => {
+//   return (
+//     <div className="px-4 flex-1 overflow-auto">
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//     </div>
+//   );
+// }
+
+// export default Messages
